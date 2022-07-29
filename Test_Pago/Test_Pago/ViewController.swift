0fa13acfb0c6b997a900f80cc81023e8e3e33e54
TeodorPloae <ploaetdr@yahoo.com>
2022-07-29 17:18:03 +0300

    create project

---
+++ Test_Pago/Test_Pago/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_Pago
+//
+//  Created by Teodor Ploae on 29.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Test_Pago/Test_PagoUITests/Test_PagoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Test_PagoUITestsLaunchTests.swift
+//  Test_PagoUITests
+//
+//  Created by Teodor Ploae on 29.07.2022.
+//
+
+import XCTest
+
+class Test_PagoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
